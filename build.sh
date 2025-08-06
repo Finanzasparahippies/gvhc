164f786ec67934c0a1da0ca601bf47105110cf0c
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2025-08-06 15:50:24 -0700

    ffmpeg instalation raw fix

--- build.sh
+++ build.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Directorio para la instalación
-mkdir -p /opt/render/project/src/ffmpeg_bin
-cd /opt/render/project/src/ffmpeg_bin
+# Directorio de instalación dentro del proyecto
+INSTALL_DIR="./ffmpeg_bin"
+mkdir -p "$INSTALL_DIR"
+cd "$INSTALL_DIR"
 
-# URL de descarga de FFmpeg para Linux
+# Descargar y extraer FFmpeg
 FFMPEG_URL="https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
 wget -O ffmpeg.tar.xz "$FFMPEG_URL"
 tar xvf ffmpeg.tar.xz
 
-# El directorio extraído tendrá un nombre como "ffmpeg-N.N-amd64-static"
-# Buscamos el directorio y extraemos el binario
-FFMPEG_DIR=$(find . -maxdepth 1 -type d -name "ffmpeg-*" -print -quit)
-cp "$FFMPEG_DIR/ffmpeg" /usr/local/bin/
-cp "$FFMPEG_DIR/ffprobe" /usr/local/bin/
+# Copiamos los binarios a la raíz de nuestro directorio de instalación
+# El nombre del subdirectorio extraído cambia con la versión, por lo que lo buscamos dinámicamente
+FFMPEG_SUBDIR=$(find . -maxdepth 1 -type d -name "ffmpeg-*" -print -quit)
+cp "$FFMPEG_SUBDIR/ffmpeg" .
+cp "$FFMPEG_SUBDIR/ffprobe" .
 
-# Limpiamos los archivos descargados
-rm -r "$FFMPEG_DIR" ffmpeg.tar.xz
+# Limpiamos los archivos temporales para reducir el tamaño final de la imagen
+rm -r "$FFMPEG_SUBDIR" ffmpeg.tar.xz
+cd ..
+
+# Agregamos el directorio de instalación al PATH del sistema para que la app lo encuentre
+export PATH="$INSTALL_DIR:$PATH"
+echo "PATH del entorno de construcción actualizado: $PATH"
 
 # Continuamos con el resto de la construcción
 pip install -r requirements.txt
