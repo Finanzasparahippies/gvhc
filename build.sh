f17a3c812f74d2dd2fa4367176c2a891fac4044b
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2025-08-06 15:48:22 -0700

    ffmpeg instalation raw

--- build.sh
+++ build.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
-# Exit script if any command fails
 set -o errexit
 
-apt-get update
-apt-get install -y ffmpeg
+# Directorio para la instalación
+mkdir -p /opt/render/project/src/ffmpeg_bin
+cd /opt/render/project/src/ffmpeg_bin
 
-python -m pip install --upgrade pip
-# Instalar dependencias de Python
+# URL de descarga de FFmpeg para Linux
+FFMPEG_URL="https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
+wget -O ffmpeg.tar.xz "$FFMPEG_URL"
+tar xvf ffmpeg.tar.xz
+
+# El directorio extraído tendrá un nombre como "ffmpeg-N.N-amd64-static"
+# Buscamos el directorio y extraemos el binario
+FFMPEG_DIR=$(find . -maxdepth 1 -type d -name "ffmpeg-*" -print -quit)
+cp "$FFMPEG_DIR/ffmpeg" /usr/local/bin/
+cp "$FFMPEG_DIR/ffprobe" /usr/local/bin/
+
+# Limpiamos los archivos descargados
+rm -r "$FFMPEG_DIR" ffmpeg.tar.xz
+
+# Continuamos con el resto de la construcción
 pip install -r requirements.txt
-
-# Descargar el modelo de spaCy necesario
 python -m spacy download en_core_web_md
-python -m spacy download es_core_news_md
-
-# Ejecutar migraciones y otros comandos de despliegue
+python -m spacy download es_core_web_md
 python manage.py collectstatic --no-input
 python manage.py migrate
