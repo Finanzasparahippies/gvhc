ef8ae3365cd07182dfc9e70141d10ee1d9e4e52b
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2025-08-06 14:23:35 -0700

    build.sh added with spanish spacy

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 # Exit script if any command fails
 set -o errexit
 
+python -m pip install --upgrade pip
 # Instalar dependencias de Python
 pip install -r requirements.txt
 
 # Descargar el modelo de spaCy necesario
 python -m spacy download en_core_web_md
+python -m spacy download es_core_web_md
 
 # Ejecutar migraciones y otros comandos de despliegue
 python manage.py collectstatic --no-input
