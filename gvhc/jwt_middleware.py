6ad4e2a2d6edd98b34d4e9244a2c691ff4216232
Finanzasparahippies <saul.redesdemercadeo@gmail.com>
2025-08-09 13:20:04 -0700

    hopefuly working on production

--- gvhc/jwt_middleware.py
+++ gvhc/jwt_middleware.py
@@ -2,6 +2,7 @@
 from channels.auth import AuthMiddleware
 from django.db import close_old_connections
 from urllib.parse import parse_qs
+
 from channels.db import database_sync_to_async
 
 @database_sync_to_async
@@ -20,24 +21,33 @@ def get_user_from_token(token):
 
 class JWTAuthMiddleware(AuthMiddleware):
     async def resolve_scope(self, scope):
-        # close_old_connections()
-        headers = dict(scope['headers'])
-        token_str = None
+        from django.contrib.auth.models import AnonymousUser
 
-        # Intenta obtener el token del encabezado Authorization
+        close_old_connections()
+        
+        token_str = None
+        user = AnonymousUser()
+
+        headers = dict(scope['headers'])
         if b'authorization' in headers:
             auth_header = headers[b'authorization'].decode('utf-8')
             if auth_header.startswith('Bearer '):
                 token_str = auth_header.split(' ')[1]
         
-        # Si no está en el encabezado, busca en la URL (como alternativa)
+        # 2. Si no se encuentra el token, buscamos en la query string
         if not token_str and 'query_string' in scope:
             query_params = parse_qs(scope['query_string'].decode())
             if 'token' in query_params:
                 token_str = query_params['token'][0]
 
-        scope['user'] = None
+        # 3. Si se encuentra un token, intentamos obtener el usuario
         if token_str:
-            scope['user'] = await get_user_from_token(token_str)
+            db_user = await get_user_from_token(token_str)
+            if db_user:
+                user = db_user
+
+        # 4. Establecemos el usuario en el scope
+        scope['user'] = user
         
+        # 5. Llamamos a la implementación del middleware base
         return await super().resolve_scope(scope)
